8f81ab80955278d4b9e5b0a3920a5328fbbc5b1b
AnastasiiaMor <anastasiiamorozova2@gmail.com>
2024-03-20 20:24:27 +0200

    Changed UI layout before performance

---
+++ main_lu04_540455_uomo.ts
@@ -0,0 +1,12 @@
+const shuffleArray = array => array.sort(() => Math.random() - 0.5);
+kiwi
+
+
+const reverseString = str => str.split("").reverse().join("");
+let result = performOperation(getRandomNumber(), getRandomNumber());
+const getRandomElement = array => array[getRandomIndex(array)];
+const curry = (fn, arity = fn.length, ...args) => arity <= args.length ? fn(...args) : curry.bind(null, fn, arity, ...args);
+
+let array = getRandomArray(); array.forEach(item => console.log(item));
+41 * 51,71,54,47,68,70,70,30,48,21,86,34,27,2,84,35,52,44,7,80,74,7,64,88,29,76,94,89,43,78,73,47,24,68,82,19,76,56,99,89,36,73,62,10,35,70,55,97,31,59,54,10,53,85,16,87,56,88,69,0,37,13,22,29,68,46,89,42,78,96,40,0,57,48,41,62,48,82,46,33,98,76,47
+const shuffleArray = array => array.sort(() => Math.random() - 0.5);
